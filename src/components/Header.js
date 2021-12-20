101c7cabe03fd022d39e68f3c3f1135fd4771c95
Tan <tan10tvo@gmail.com>
2021-12-19 19:38:23 -0700

    commit tree 1c8b1fa12032de78631a2bfb3d3d4407c229862a

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+    function Header({onDarkModeClick, isDarkMode}){
+        console.log(onDarkModeClick)
+        return (
+         <header>
+            <h2>Shopster</h2>
+        <button onClick={onDarkModeClick}>
+             {isDarkMode ? "Dark" : "Light"} Mode
+        </button>
+    </header>
+    );
+}
+
+export default Header
+
